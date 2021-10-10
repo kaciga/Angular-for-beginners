fb3a21ac08c0e25864d61f0c8a8d65585e0d2235
Tóthné Pásztor Mónika <kaciga82@gmail.com>
2021-10-10 09:39:11 +0200

    6-15. section of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {COURSES} from '../db-data';
+import { Course } from './model/course';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,23 @@ import {COURSES} from '../db-data';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  courses = COURSES;
+
+  title = COURSES[0].description;
+  
+  price = 9.923699;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
+  startDate = new Date(2000, 0, 1);
+
+  onCourseSelected(course:Course) {
+    console.log("App component - click event bubbled...", course);
+
+  }
 
 
 
--- src/db-data.ts
+++ src/db-data.ts
@@ -4,10 +4,9 @@ export const COURSES: any = [
 
     {
         id: 1,
-        description: "Angular Core Deep Dive",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        description: "Angular core deep dive",
         longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-        category: 'INTERMEDIATE',
+        // category: 'INTERMEDIATE', - ha nincs ez a sor, jön az ngswitchdefault..
         lessonsCount: 10
     },
     {
